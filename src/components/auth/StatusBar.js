fde4a051c189c8f29ebe4f453a49eabcba1b1cd1
daineto <ainetopro@gmail.com>
2024-09-16 17:07:37 +0200

    Account details page created

--- src/components/auth/StatusBar.js
+++ src/components/auth/StatusBar.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 import './StatusBar.css';
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"></link>
 
 const StatusBar = () => {
   const { isAuthenticated, userName, logout } = useAuth();
@@ -29,6 +30,10 @@ const StatusBar = () => {
 
   const handleSearchChange = (e) => {
     setSearchValue(e.target.value);
+  };
+
+  const handleAccountDetailRedirect = () => {
+    navigate('/accountDetail');
   };
 
   return (
@@ -65,6 +70,12 @@ const StatusBar = () => {
           <div className="statusbar-auth">
             <span className="user-name">Welcome, {userName}!</span>
             <button onClick={handleLogout} className="logout-button">Logout</button>
+            <button onClick={handleAccountDetailRedirect} className="">
+              <svg className="btn-accountDetail" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path fill="#ffffff" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"/>
+              </svg>
+            </button>
+
           </div>
         ) : (
           <div className="statusbar-auth">
